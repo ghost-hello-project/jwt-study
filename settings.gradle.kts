92cb9e5f0beb3704686344c937ff8d63fcfad32c
laolang2016 <xiaodaima2016@163.com>
2024-09-21 03:04:13 +0800

    初始化项目

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        // 插件使用阿里云 maven 源
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "jwt-study"
+
